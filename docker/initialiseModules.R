093a9321e591818d0c13820f604b13b8d6dbef6b
egillax <egillax@gmail.com>
2024-03-25 13:55:31 +0100

    upgrade modules

--- docker/initialiseModules.R
+++ docker/initialiseModules.R
@@ -1,4 +1,4 @@
-json <- paste(readLines("deep_comp_study.json"), collapse = "\n")
+json <- paste(readLines("./study_execution_jsons/deep_comp_study.json"), collapse = "\n")
 analysisSpecifications <- ParallelLogger::convertJsonToSettings(json)
 Sys.setenv("INSTANTIATED_MODULES_FOLDER" = "/modules")
 Strategus::ensureAllModulesInstantiated(analysisSpecifications)
