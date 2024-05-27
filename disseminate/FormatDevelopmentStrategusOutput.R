05f227fa684bd22796e6594bd036b7b392831ced
Henrik John <l.john@erasmusmc.nl>
2024-05-27 17:42:39 +0200

    Add plotting functions

---
+++ disseminate/FormatDevelopmentStrategusOutput.R
@@ -0,0 +1,58 @@
+library(dplyr)
+
+getAllAuc <- function(strategusOutputPath) {
+  
+  combinedData <- NULL
+  subdirs <- list.files(strategusOutputPath, full.names = TRUE)
+  
+  for (dir in subdirs) {
+    folder <- basename(dir)
+    print(basename(folder))
+    
+    allFiles <- list.files(dir, pattern = "models.csv", full.names = TRUE, recursive = TRUE)
+    
+    for(modelFilePath in allFiles) {
+      # Assume the same naming pattern and location for database_details.csv and database_meta_data.csv
+      directoryPath <- dirname(modelFilePath)
+      databaseDetailsPath <- file.path(directoryPath, "database_details.csv")
+      databaseMetaDataPath <- file.path(directoryPath, "database_meta_data.csv")
+      evalDataPath <- file.path(directoryPath, "evaluation_statistics.csv")
+      modelDesign <- file.path(directoryPath, "model_designs.csv")
+      cohorts <- file.path(directoryPath, "cohorts.csv")
+      
+      # Read models.csv
+      modelData <- read.csv(modelFilePath)
+      databaseDetails <- read.csv(databaseDetailsPath)
+      databaseMetaData <- read.csv(databaseMetaDataPath)
+      evalData <- read.csv(evalDataPath)
+      modelDesign <- read.csv(modelDesign)
+      cohorts <- read.csv(cohorts)
+      
+      enrichedData <- merge(modelData, databaseDetails, by = "database_id")
+      finalModelData <- merge(enrichedData, databaseMetaData, by.y = "database_id", by.x = "database_meta_data_id")
+      finalModelData <- merge(finalModelData, modelDesign, by = "model_design_id")
+      finalModelData <- merge(finalModelData, cohorts, by.x = "outcome_id", by.y = "cohort_id")
+      
+      
+      evalData <- evalData |>
+        dplyr::filter(metric == "AUROC", evaluation == "Test")
+      
+      finalModelData <- merge(finalModelData, evalData, by.x = "model_id", by.y = "performance_id")
+      
+      # Combine with previous iterations' data
+      if(is.null(combinedData)) {
+        combinedData <- finalModelData
+      } else {
+        combinedData <- rbind(combinedData, finalModelData)
+      }
+    }
+  }
+  
+  finalSelectedData <- combinedData %>%
+    select(cdm_source_abbreviation, database_meta_data_id, model_id, analysis_id, model_design_id, model_type, metric, value, cohort_definition_id, cohort_name)
+  # ensure development and validation database are the same for internal validation
+  finalSelectedData$validation <- finalSelectedData$cdm_source_abbreviation
+  finalSelectedData$validation_database_meta_data_id <- finalSelectedData$database_meta_data_id
+  
+  return(finalSelectedData)
+}
---
+++ disseminate/FormatValidationStrategusOutput.R
@@ -0,0 +1,70 @@
+library(dplyr)
+library(readr)
+
+getAllAucValidation <- function(strategusOutputPath) {
+  
+  combinedData <- NULL
+  subdirs <- list.files(strategusOutputPath, full.names = TRUE)
+  
+  for (dir in subdirs) {
+    folder <- basename(dir)
+    print(basename(folder))
+    
+    allFiles <- list.files(dir, pattern = "performances.csv", full.names = TRUE, recursive = TRUE)
+    
+    for(performancesFilePath in allFiles) {
+      # Assume the same naming pattern and location for database_details.csv and database_meta_data.csv
+      directoryPath <- dirname(performancesFilePath)
+      
+      evaluationStatisticPath <- file.path(directoryPath, "evaluation_statistics.csv")
+      databaseDetailsPath <- file.path(directoryPath, "database_details.csv")
+      cohorts <- file.path(directoryPath, "cohorts.csv")
+      
+      
+      # databaseMetaDataPath <- file.path(directoryPath, "database_meta_data.csv")
+      # evalDataPath <- file.path(directoryPath, "evaluation_statistics.csv")
+      modelDesign <- file.path(directoryPath, "model_designs.csv")
+      # cohorts <- file.path(directoryPath, "cohorts.csv")
+      modelSettings <- file.path(directoryPath, "model_settings.csv")
+      
+      
+      # Read models.csv
+      performancesData <- read.csv(performancesFilePath)
+      evaluationStatisticData <- read.csv(evaluationStatisticPath)
+      evaluationStatisticData <- evaluationStatisticData[evaluationStatisticData$metric == "AUROC", ]
+      
+      databaseDetails <- read.csv(databaseDetailsPath)
+      cohorts <- read.csv(cohorts)
+      
+      modelSettings <- read_csv(modelSettings, col_types = cols(model_setting_id = "n", model_type = "c", model_settings_json = col_skip()))
+      
+      # databaseMetaData <- read.csv(databaseMetaDataPath)
+      # evalData <- read.csv(evalDataPath)
+      modelDesign <- read.csv(modelDesign)
+      
+      finalValidationData <- merge(performancesData, evaluationStatisticData, by = "performance_id")
+      finalValidationData <- merge(finalValidationData, databaseDetails, by.y = "database_id", by.x = "validation_database_id")
+      colnames(finalValidationData)[colnames(finalValidationData) == "database_meta_data_id"] <- "validation_database_meta_data_id"
+      finalValidationData <- merge(finalValidationData, databaseDetails, by.y = "database_id", by.x = "development_database_id")
+      colnames(finalValidationData)[colnames(finalValidationData) == "database_meta_data_id"] <- "database_meta_data_id"
+      
+      finalValidationData$outcome_id <- NULL
+      finalValidationData$target_id <- NULL
+      
+      finalValidationData <- merge(finalValidationData, modelDesign, by = "model_design_id")
+      finalValidationData <- merge(finalValidationData, modelSettings, by = "model_setting_id")
+      finalValidationData <- merge(finalValidationData, cohorts, by.x = "outcome_id", by.y = "cohort_id")
+      
+      # Combine with previous iterations' data
+      if(is.null(combinedData)) {
+        combinedData <- finalValidationData
+      } else {
+        combinedData <- rbind(combinedData, finalValidationData)
+      }
+    }
+  }
+  
+  finalSelectedData <- combinedData %>%
+    select(database_meta_data_id, validation_database_meta_data_id, model_design_id, model_type, metric, value, cohort_definition_id, cohort_name) %>%
+    filter(database_meta_data_id != validation_database_meta_data_id)
+}
